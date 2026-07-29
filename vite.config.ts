0d32a3b668d3a04a679f0412fc5a39418d9c4fc0
InfectedNostril <mokshith.mxc@gmail.com>
2026-07-29 14:42:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Game_theory_assignment/', // Adds base path for GitHub Pages
   plugins: [
     react(),
     tailwindcss(),
